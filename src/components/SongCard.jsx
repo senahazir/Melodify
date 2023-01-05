32d96e64590b0acac0700192c23187a699049eb5
Sena Gul Hazir <senahazir@berkeley.edu>
2023-01-04 19:01:54 -0800

    play/pause done

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import  PlayPause  from './PlayPause';
+import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
+  const dispatch = useDispatch();
   const handlePauseClick = () => {
-
+    dispatch(playPause(false));
   };
 
   const handlePlayClick = () => {
-
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
   };
-
-
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
