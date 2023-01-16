654e451e85d5de7cabbde9b3d164f3aea00caf4d
Sena Gul Hazir <senahazir@berkeley.edu>
2023-01-16 00:37:46 -0800

    discover bugged

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,9 +11,32 @@ import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
-const TopChartCard = ({ song, id }) => (
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick}) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2"> 
-    {song.title}
+    <h3 className='font-bol text-white text-base mr-3'>{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center ">
+      <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex flex-col flex-1 mx-3">
+        <Link to={`/songs/${song.key}`}className="text-white font-bold text-xl">
+        <p>
+          {song?.title}
+        </p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamId}`} className="text-gray-300 text-base mt-1">
+        <p>
+          {song?.subtitle}
+        </p>
+        </Link>
+      </div>
+    </div>
+    <PlayPause
+    isPlaying={isPlaying}
+    activeSong={activeSong}
+    song={song}
+    handlePause={handlePauseClick}
+    handlePlay={handlePlayClick}
+  
+    />
   </div>
 
 )
@@ -34,7 +57,7 @@ const TopPlay = () => {
     dispatch(playPause(false));
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -56,11 +79,45 @@ const TopPlay = () => {
               key={song.key}
               song={song}
               i={i}
-              
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => (handlePlayClick(song, i))}
+
             />
           ))}
-
         </div>
+      </div>
+      <div className="w-full mt-8 flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">
+            Top Artists
+          </h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See More</p>
+          </Link>
+        </div>
+        <Swiper
+        slidesPerView="auto"
+        spaceBetween={15}
+        freeMode
+        centeredSlides
+        centeredSlidesBounds
+        modules={[FreeMode]}
+        className="mt-4"
+        >
+          {topPlays?.map((song, id) => (
+          <SwiperSlide
+           key = {song.key}
+           style= {{width: '20%', height: 'auto'}}
+           className='shadow-lg animate-slide'>
+            <Link to={`/artists/${song?.artists[0].adamId}`}>
+            <img src={song?.images.background} alt="name" 
+          className="w-full object-cover rounded-full"/>
+            </Link>
+          </SwiperSlide>
+        ))}
+        </Swiper>
       </div>
     </div>
   );
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -20,18 +20,18 @@ const Discover = () => {
   console.log(data);
   return (
     <div className="flex flex-col">
-      <div className="w-full flex justify-between items-center sm: flex-row flex-col mt-4 mb-10">
-        <h2 className="text-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
         <select
           onChange={() => {}}
           value=""
-          className="bg-black text-gray-300 p-3 text-sm-rounded-lg outline-none sm:mt-0 mt-5"
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
         </select>
       </div>
 
-      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+      <div className="flex flex-wrap sm:justify-start justify-center gap-2">
         {data?.map((song, i) => (
           <SongCard
             key={song.key}
